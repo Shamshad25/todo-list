e1222a58d74553ec02eace026cea0477ae15ba8e
Shamshad Alam <70852128+Shamshad25@users.noreply.github.com>
2023-10-08 17:11:15 +0530

    cleanup

--- src/components/EditModal.tsx
+++ src/components/EditModal.tsx
@@ -8,18 +8,18 @@ import {
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { updateTask, todoSelector } from "../redux/feature/todoSlice";
 
-const customStyles = {
-  content: {
-    width: "50%",
-    // height: "30%",
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
+// const customStyles = {
+//   content: {
+//     width: "50%",
+//     // height: "30%",
+//     top: "50%",
+//     left: "50%",
+//     right: "auto",
+//     bottom: "auto",
+//     marginRight: "-50%",
+//     transform: "translate(-50%, -50%)",
+//   },
+// };
 
 interface Iid {
   id: string;
